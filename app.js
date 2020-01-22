e6584d760256df47a8c6656a5c446586687a7530
RK <robert.khnkoyan"gmail.com>
2020-01-22 22:44:11 +0000

    06 контроллеры

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var path = require('path');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Import routes for "catalog" area of site
+var catalogRouter = require('./routes/catalog');  
+/////////////////////////////////
+
 var app = express();
 ////////////////////////////////////////////////////
 //Устанавливаем соединение с mongoose
@@ -35,6 +39,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Կարծես էստեղ ռոուտերները ինիցիլիզացվում են
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Add catalog routes to middleware chain.
+app.use('/catalog', catalogRouter);  
 
 //////////////////////////////////////////////////
 // catch 404 and forward to error handler
