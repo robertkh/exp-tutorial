ee95d04b0bbb553c32ba10ffc0334af69941bacf
RK <robert.khnkoyan"gmail.com>
2020-01-23 11:55:06 +0000

    08 Home page updated

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,10 +1,35 @@
-var Book = require('../models/book');
-
 //////////////////////////////////////////////////
-//   index
+//   index - home_page
 
-exports.index = function(req, res) {
-    res.render('index', { title: 'Express' });
+var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
+
+var async = require('async');
+
+exports.index = function(req, res) {   
+    
+    async.parallel({
+        book_count: function(callback) {
+            Book.countDocuments({}, callback); 
+        },
+        book_instance_count: function(callback) {
+            BookInstance.countDocuments({}, callback);
+        },
+        book_instance_available_count: function(callback) {
+            BookInstance.countDocuments({status:'Available'}, callback);
+        },
+        author_count: function(callback) {
+            Author.countDocuments({}, callback);
+        },
+        genre_count: function(callback) {
+            Genre.countDocuments({}, callback);
+        }
+    }, function(err, results) {
+        res.render('index', { title: 'Local Library Home', error: err, data: results });
+        //console.log(results);
+    });
 };
 
 ////////////////////////////////////////////////////
