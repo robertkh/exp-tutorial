3a9b5cf8101e731959ce6685c4e5dec548ce6e35
Robert Khnkoyan <robert.khnkoyan@gmail.com>
2021-01-10 14:14:53 +0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 //Устанавливаем соединение с mongoose
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://robert:parnarareg3@cluster0.exdyz.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://robertkh:parnarareg@cluster0.jmjzr.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
